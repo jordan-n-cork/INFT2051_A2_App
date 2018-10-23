88a954e8295d1a182d47474cd9b63b671ffb8f00
thejrosso <thejrosso@gmail.com>
2018-10-23 13:52:31 +1100

    TodayView as XAML layout

--- A2_AppProject/A2_AppProject/A2_AppProject/obj/Debug/netstandard2.0/Pages/TodayView.xaml.g.cs
+++ A2_AppProject/A2_AppProject/A2_AppProject/obj/Debug/netstandard2.0/Pages/TodayView.xaml.g.cs
@@ -17,8 +17,12 @@ namespace A2_AppProject.Pages {
     public partial class TodayView : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.ListView TodayViewList;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(TodayView));
+            TodayViewList = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "TodayViewList");
         }
     }
 }
